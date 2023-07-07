1bd30704dad13435f2ed2dc81345f861c7b45380
iamsadikrahman <iamsadikrahman@outlook.com>
2023-07-07 15:01:27 +0600

    paddy and potato section added

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -14,13 +14,13 @@
         <div class="season-calculation">
 
           <label for="potato-field">আলুর মোট জমির পরিমাণঃ</label>
-          <input type="text" name="potato-field">
+          <input type="text" name="potato-field" v-model="potatoField">
           <br>
           <label for="potato-per-unit-cost">আলু প্রতি শতকঃ</label>
-          <input type="text" name="potato-per-unit-cost" value="25">
+          <input type="text" name="potato-per-unit-cost" v-model="potatoPerUnitCost">
 
           <br>
-          <h4 class="potato-total-cost">আলুর মোট সেচ খরচঃ ০০ টাকা</h4>
+          <h4 class="potato-total-cost">আলুর মোট সেচ খরচঃ {{ potatoTotalCost }} টাকা</h4>
 
         </div>
     </section>
@@ -29,13 +29,13 @@
       <div class="season-calculation">
 
         <label for="paddy-field">ইরির মোট জমির পরিমাণঃ</label>
-        <input type="text" name="paddy-field">
+        <input type="text" name="paddy-field" v-model="paddyField">
         <br>
         <label for="paddy-per-unit-cost">ইরির প্রতি শতকঃ</label>
-        <input type="text" name="paddy-per-unit-cost" value="45">
+        <input type="text" name="paddy-per-unit-cost" v-model="paddyPerUnitCost">
 
         <br>
-        <h4 class="paddy-total-cost">ইরির মোট সেচ খরচঃ ০০ টাকা</h4>
+        <h4 class="paddy-total-cost">ইরির মোট সেচ খরচঃ {{ paddyTotalCost }} টাকা</h4>
 
       </div>
     </section>
@@ -44,7 +44,46 @@
 
 <script setup>
 
+import {ref, computed, watch} from "vue";
+
+const potatoField = ref('')
+const paddyField = ref('')
+const potatoPerUnitCost = ref(25)
+const paddyPerUnitCost = ref(45)
+let totalPotatoCost;
+let totalPaddyCost;
+
+
+// Potato Calculation
+const potatoTotalCost = computed(()=>{
+  const totalPotatoField = parseInt(potatoField.value)
+  const perUnitCostPotato = potatoPerUnitCost.value
+
+  if (isNaN(totalPotatoField) || isNaN(perUnitCostPotato)) {
+    totalPotatoCost = 0
+    return totalPotatoCost;
+  }
+  totalPotatoCost = totalPotatoField * perUnitCostPotato;
+  return totalPotatoCost
+})
+
+// Paddy Calculation
+const paddyTotalCost  = computed(()=>{
+  const totalPaddyField = parseInt(paddyField.value)
+  const perUnitCostPaddy = paddyPerUnitCost.value
+
+  if(isNaN(totalPaddyField) || isNaN(perUnitCostPaddy)){
+    totalPaddyCost = 0
+    return totalPaddyCost
+  }
+  totalPaddyCost = totalPaddyField * perUnitCostPaddy
+  return totalPaddyCost
+
+})
+
 </script>
+
+
 
 <style scoped>
 
@@ -53,7 +92,7 @@
   padding: 8px 2px;
   border-radius: 5px;
   background-color: darkgrey;
-  font-size: 22px;
+  font-size: 20px;
 }
 
 .halkhata {
