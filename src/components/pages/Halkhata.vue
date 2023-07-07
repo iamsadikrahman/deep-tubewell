46483679543c3c6cb92635680ee767c04d394aaf
iamsadikrahman <iamsadikrahman@outlook.com>
2023-07-07 23:37:56 +0600

    input type added

--- src/components/pages/Halkhata.vue
+++ src/components/pages/Halkhata.vue
@@ -12,12 +12,12 @@
 
           <div>
             <label class="block mb-3">
-              <span class="block text-sm font-medium text-slate-700">আলুর মোট জমির পরিমাণঃ</span>
+              <span class="block text-sm font-medium text-slate-700">আলুর মোট জমির পরিমাণঃ (শতক)</span>
               <input type="text" name="potato-field" v-model="potatoField" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" >
             </label>
 
             <label class="block">
-              <span class="block text-sm font-medium text-slate-700">আলু প্রতি শতক সেচ খরচঃ</span>
+              <span class="block text-sm font-medium text-slate-700">আলু প্রতি শতক সেচ খরচঃ (টাকা)</span>
               <input type="text" name="potato-per-unit-cost" v-model="potatoPerUnitCost" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
             </label>
           </div>
@@ -38,12 +38,12 @@
 
         <div>
           <label class="block mb-3">
-            <span class="block text-sm font-medium text-slate-700">ইরির মোট জমির পরিমাণঃ</span>
+            <span class="block text-sm font-medium text-slate-700">ইরির মোট জমির পরিমাণঃ (শতক)</span>
             <input type="text" name="potato-field" v-model="paddyField" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" >
           </label>
 
           <label class="block">
-            <span class="block text-sm font-medium text-slate-700">ইরি প্রতি শতক সেচ খরচঃ</span>
+            <span class="block text-sm font-medium text-slate-700">ইরি প্রতি শতক সেচ খরচঃ (টাকা)</span>
             <input type="text" name="potato-per-unit-cost" v-model="paddyPerUnitCost" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
           </label>
         </div>
@@ -64,7 +64,7 @@
           <span class="block text-sm font-medium text-black">মোট সেচ খরচঃ <span class="rounded-full bg-emerald-300 px-2 py-2">{{totalCost}} ৳</span></span>
 
           <label class="block">
-            <span class="block text-sm font-medium text-slate-700">অগ্রীম জমার পরিমাণঃ</span>
+            <span class="block text-sm font-medium text-slate-700">অগ্রীম জমার পরিমাণঃ (টাকা)</span>
             <input type="text" name="advance-paid" v-model="advancePaid" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
           </label>
         </div>
@@ -73,7 +73,7 @@
           <span class="block text-sm font-medium text-slate-700 ">বর্তমান বকেয়াঃ <span class="rounded-full bg-red-200 px-2 py-2 text-black">{{totalCurrentDue}} ৳</span></span>
 
           <label class="block">
-            <span class="block text-sm font-medium text-slate-700">হালখাতায় জমার পরিমাণঃ</span>
+            <span class="block text-sm font-medium text-slate-700">হালখাতায় জমার পরিমাণঃ (টাকা)</span>
             <input type="text" name="halkhata-paid" v-model="halkhataPaid" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300  focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
           </label>
         </div>
